2433b4e60cebcba2ce9369c112bb9cdb2c160a31
John Strickler <jstrickler@gmail.com>
2026-08-20 07:57:34 -0400

    monday AM

--- ANSWERS/energy_use_plot.py
+++ ANSWERS/energy_use_plot.py
@@ -3,7 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-col_names = ["1960","1965","1970","1975","1980","1985","1990","1991",
+col_names = ["DESC", "1960","1965","1970","1975","1980","1985","1990","1991",
     "1992","1993","1994","1995","1996","1997","1998","1999","2000",
     "2001","2002","2003","2004","2005","2006","2007","2008","2009",
     "2010","2011"
@@ -13,7 +13,7 @@ df = pd.read_csv(
     '../DATA/energy_use_quad.csv',
     names=col_names,
     index_col=0,
-    usecols=[0, 1, 2, 3, 4, 5, 6, 7],
+    # usecols=[0, 1, 2, 3, 4, 5, 6, 7],
 )
 
 
@@ -24,10 +24,8 @@ d2 = df.transpose()
 d2.columns = df.index
 print(d2.columns)
 print(d2)
-plt.plot(d2['Residential and commercial'])
-plt.show()
 
 for header in 'Transportation', 'Industrial', 'Residential and commercial':
-    plt.plot(df2[header], label=header)
+    plt.plot(d2[header], label=header)
 plt.legend()
 plt.show()
